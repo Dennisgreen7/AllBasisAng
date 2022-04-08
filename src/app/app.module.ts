fc6ae439a3d15fcc4c814f8d9d7c4335af71c234
Dennisgreen7 <denis4167@gmail.com>
2022-04-08 19:52:22 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quiz/quiz.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent implements OnInit {
+  results: any = {};
+  couter: number = 0;
+  isGood: boolean = false;
+  showQuiz: boolean = true;
+  showResult: boolean = false;
+  result:string = "";
+  imgGood:string = "https://cdn.boldomatic.com/content/post/hEuyNw/You-re-a-good-person-and-you-know-it-so-don-t-was?size=800";
+  imgBad:string = "https://memegenerator.net/img/instances/36404317.jpg";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  handleSumbit(formData: any) {
+    this.results = formData;
+    if (this.results.Q1 == '1') {
+      this.couter++;
+    }
+    if (this.results.Q2 == '1') {
+      this.couter++;
+    }
+    if (this.results.Q3 == '1') {
+      this.couter++;
+    }
+    if (this.couter > 1) {
+      this.isGood = true;
+      this.result='Good person';
+    }
+    else{
+      this.result='Bad person';
+    }
+    alert(this.result);
+    this.showQuiz = false;
+    this.showResult = true;
+  }
+ //// localStorage.setItem('result', this.result='Bad person');
+}
